3e3b8c80ca0cdf82632aeb736316887d7dd0655a
Martha Gimenez <martha.gimenez@gmail.com>
2024-11-06 08:04:24 -0300

    Version1

---
+++ Modulo1.py
@@ -0,0 +1,16 @@
+"""Modulo 1: Clase en Python que guarda un valor entero"""
+class Modulo1:
+    valor = 0
+
+    """Inicializa la clase Modulo1"""
+    def __init__(self,valor):
+        self.valor = valor
+
+    """"Imprime la clase Modulo1"""
+    def printValor(self):
+        print("Modulo 1: " + str(self.valor))
+
+
+if __name__ == '__main__':
+    modulo = Modulo1(10)
+    modulo.printValor()
